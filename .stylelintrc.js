400f0a4d4856fa256d7ac45797bd0bdcc8482aac
dvader33 <vhader33.dr@gmail.com>
2018-01-15 17:01:44 -0600

    at-rules

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -2,13 +2,17 @@ module.exports = {
   "extends": "stylelint-config-standard",
   "rules": {
     "indentation": 2,
+    "max-nesting-depth": 3,
     "at-rule-no-unknown": [ true, {
       "ignoreAtRules": [
         "extends",
         "extend",
         "ignores",
         "mixin",
-        "include"
+        "include",
+        "content",
+        "if",
+        "else"
       ]
     }],
   }
